d56b7dd75ed93ba5a04bcaaeefebd950a54196b7
MalaySingh <malaysingh121993@gmail.com>
2017-06-08 14:02:55 +0200

    same with minor bug fixes

--- optimize.py
+++ optimize.py
@@ -17,21 +17,14 @@ def cost_func(x, P, U_evolution, U_target):
     infidelity = 1- F
     return infidelity
 
-def cost_func(x, P, U_evolution, U_target):
-    """
-    x : array like
-    x[0] : parameter for exp(i*sigmaz_A*x[0])
-    x[1] : parameter for exp(i*sigmaz_B*x[1])
-    x[2] : parameter for exp(i*identity*x[1])
-    this function calls fidelity and outputs 1-fidelity as a cost function
-    this is fed into an optimizer
-    """
-    theta1 = x[0]
-    theta2 = x[1]
-    theta3 = x[3]
-    unitary_product_phase = matrix_optimize(theta1, theta2, theta3)
-    unitary = unitary_product_phase*U_evolution
-    projected_unitary = P*unitary*P.dag()
-    F = fidelity(U_target, projected_unitary)
-    infidelity = 1- F
-    return infidelity
+import scipy as scipy
+import scipy.optimize as optimize
+
+#optimizer
+
+#anonymous call, vary only x
+infidelity = lambda x: cost_func(x, P = P, U_evolution = U_evolution, U_target = U_target)
+#setup the constraints
+bnds = ((0,2*np.pi),(0,2*np.pi),(0,2*np.pi))
+x0 = [0,0,0]
+res = scipy.optimize.minimize(infidelity, x0, method= 'BFGS',bounds=bnds, tol= 1e-1)
